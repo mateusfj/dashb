0ad727840581bea27026b8c1ba0d0102d06b76c0
Mateus <93952397+mateusfj@users.noreply.github.com>
2024-04-10 12:50:58 -0300

    update content

--- home.php
+++ home.php
@@ -16,56 +16,216 @@
 
     <div id="home">
         <div class="menu-lateral">
-            <div class="logo-icon display-flex">
-                <a href=""><img src="assets/img/logo-login.png" alt="logo"></a>
-                <a href="#"><i class="bi bi-layout-sidebar-reverse"></i></a>
-            </div>
+            <div class="between">
+                <div class="logo-icon display-flex">
+                    <a href=""><img src="assets/img/logo-login.png" alt="logo"></a>
+                    <a href="#"><i class="bi bi-layout-sidebar-reverse"></i></a>
+                </div>
 
-            <div class="nav-bar">
-                <p>Favorites</p>
-                <ul>
-                    <li><a href="#"><i class="bi bi-file-text font-weight-bold"></i> Technical Docs</a></li>
-                    <li><a href="#"><i class="bi bi-file-text"></i> Campaing Guiedliness</a></li>
-                    <li><a href="#"><i class="bi bi-file-text"></i> Important Rules</a></li>
-                    <li><a href="#"><i class="bi bi-camera-video"></i> Onboarding</a></li>
-                </ul>
-            </div>
+                <div class="nav-bar">
+                    <p class="p-style">Favorites</p>
+                    <ul>
+                        <li><a href="#"><i class="bi bi-file-text font-weight-bold"></i> Technical Docs</a></li>
+                        <li><a href="#"><i class="bi bi-file-text"></i> Campaing Guiedliness</a></li>
+                        <li><a href="#"><i class="bi bi-file-text"></i> Important Rules</a></li>
+                        <li><a href="#"><i class="bi bi-camera-video"></i> Onboarding</a></li>
+                    </ul>
+                </div>
 
-            <div class="nav-bar">
-                <p>Main Menu</p>
-                <ul>
-                    <li><a href="#"><i class="bi bi-house"></i> Dashboard</a></li>
-                    <li><a href="#"><i class="bi bi-pie-chart"></i> Campaigns</a></li>
-                    <li><a href="#"><i class="bi bi-chat"></i> Chat</a></li>
-                    <li><a href="#"><i class="bi bi-life-preserver"></i> Suporte Center</a></li>
-                    <li><a href="#"><i class="bi bi-coin"></i> Leads</a></li>
-                    <li><a href="#"><i class="bi bi-folder"></i> Archive</a></li>
-                </ul>
+                <div class="nav-bar">
+                    <p class="p-style">Main Menu</p>
+                    <ul>
+                        <li><a href="#"><i class="bi bi-house"></i> Dashboard</a></li>
+                        <li><a href="#"><i class="bi bi-pie-chart"></i> Campaigns</a></li>
+                        <li><a href="#"><i class="bi bi-chat"></i> Chat</a></li>
+                        <li><a href="#"><i class="bi bi-life-preserver"></i> Suporte Center</a></li>
+                        <li><a href="#"><i class="bi bi-coin"></i> Leads</a></li>
+                        <li><a href="#"><i class="bi bi-folder"></i> Archive</a></li>
+                    </ul>
+                </div>
             </div>
-
-            <div class="perfil">
-                <div class="img-adm">
-                    <img src="assets/img/perfil.png" alt="imagem de perfil">
-                    <div>
-                        <h3>Mateus França</h3>
-                        <h6>Administrador</h6>
+            <div class="between">
+                <div class="perfil">
+                    <div class="img-adm">
+                        <img src="assets/img/perfil1.png" alt="imagem de perfil">
+                        <div>
+                            <h3>Mateus França</h3>
+                            <h6>Administrador</h6>
+                        </div>
+                    </div>
+                    <div class="escuro-mode">
+                        <a href=""><i class="bi bi-moon"></i></a>
                     </div>
                 </div>
-                <div class="escuro-mode">
-                    <a href=""><i class="bi bi-moon"></i></a>
+            </div>
+        </div>
+
+
+        <div class="centro">
+            <div class="cabecalho">
+                <div class="links">
+                    <a href="#">Campaigns /</a>
+                    <a href="#">Analytics</a>
+                </div>
+                <div class="procurar">
+                    <div>
+
+                    </div>
+                    <div>
+                        <i class="bi bi-bell"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="grafico">
+                <h1>Your total revenue</h1>
+                <div class="filtro">
+                    <h2>$90,239.00</h2>
+                    <button><i class="bi bi-calendar4"></i> Select Dates</button>
+                    <button><i class="bi bi-funnel-fill"></i> Filters</button>
                 </div>
             </div>
         </div>
 
-        <div class="display-flex">
-            <div class="cabecalho">
 
+        <div class="direita">
+            <p class="p-style">Recent Documents</p>
+            <div class="perfil">
+                <div class="img-adm">
+                    <img class="img-doc" src="assets/img/logo-login.png" alt="imagem de perfil">
+                    <div>
+                        <h3>Summer Campaing</h3>
+                        <h6>Update 2 minutes ago</h6>
+                    </div>
+                </div>
             </div>
-            <div class="conteudo">
-
+            <div class="perfil">
+                <div class="img-adm">
+                    <img class="img-doc" src="assets/img/logo-login.png" alt="imagem de perfil">
+                    <div>
+                        <h3>Inspiration Notes</h3>
+                        <h6>Update 5 hours ago</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil">
+                <div class="img-adm">
+                    <img class="img-doc" src="assets/img/logo-login.png" alt="imagem de perfil">
+                    <div>
+                        <h3>Campaing Moodboard</h3>
+                        <h6>Update 8 hours ago</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil">
+                <div class="img-adm">
+                    <img class="img-doc" src="assets/img/logo-login.png" alt="imagem de perfil">
+                    <div>
+                        <h3>Circular Inpiration</h3>
+                        <h6>Update 2 days ago</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil">
+                <div class="img-adm">
+                    <img class="img-doc" src="assets/img/logo-login.png" alt="imagem de perfil">
+                    <div>
+                        <h3>Untitled</h3>
+                        <h6>Update 2 days ago</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil">
+                <div class="img-adm">
+                    <img class="img-doc" src="assets/img/logo-login.png" alt="imagem de perfil">
+                    <div>
+                        <h3>Test Note</h3>
+                        <h6>Update 2 days ago</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil">
+                <div class="img-adm">
+                    <img class="img-doc" src="assets/img/logo-login.png" alt="imagem de perfil">
+                    <div>
+                        <h3>Concepts</h3>
+                        <h6>Update 2 days ago</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil">
+                <div class="img-adm">
+                    <img class="img-doc" src="assets/img/logo-login.png" alt="imagem de perfil">
+                    <div>
+                        <h3>Luxury Campaing Steps</h3>
+                        <h6>Update 5 days ago</h6>
+                    </div>
+                </div>
+            </div>
+            <p class="p-style">Team Mates</p>
+            <div class="perfil">
+                <div class="img-adm">
+                    <img src="assets/img/perfil1.png" alt="imagem de perfil">
+                    <div>
+                        <h3>Ethan</h3>
+                        <h6>Product Manager</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil">
+                <div class="img-adm">
+                    <img src="assets/img/perfil1.png" alt="imagem de perfil">
+                    <div>
+                        <h3>Simone Deniels</h3>
+                        <h6>Marketing Maneger</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil">
+                <div class="img-adm">
+                    <img src="assets/img/perfil1.png" alt="imagem de perfil">
+                    <div>
+                        <h3>Noah Martinez</h3>
+                        <h6>CRM Designer</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil">
+                <div class="img-adm">
+                    <img src="assets/img/perfil1.png" alt="imagem de perfil">
+                    <div>
+                        <h3>Olivia Mitchell</h3>
+                        <h6>Designer</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil">
+                <div class="img-adm">
+                    <img src="assets/img/perfil1.png" alt="imagem de perfil">
+                    <div>
+                        <h3>Liam johnson</h3>
+                        <h6>Designer</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil">
+                <div class="img-adm">
+                    <img src="assets/img/perfil1.png" alt="imagem de perfil">
+                    <div>
+                        <h3>Mateus Sousa</h3>
+                        <h6>programmer</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil">
+                <div class="img-adm">
+                    <img src="assets/img/perfil1.png" alt="imagem de perfil">
+                    <div>
+                        <h3>Avam Willians</h3>
+                        <h6>Designer</h6>
+                    </div>
+                </div>
             </div>
         </div>
-
     </div>
 
 </body>
